29cf480a709f280826f90eb2c28d097f2ea661d9
Duy Lâm <thanhduyuth@gmail.com>
2025-05-17 20:21:30 +0700

    Tạo model account admin

---
+++ models/accountAdmin.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const schema = new Schema(
+  {
+    fullName: String,
+    email: String,
+    password: String,
+    avatar: String,
+    phone: String,
+    address: String,
+    status: String,
+    deleted: {
+      type: Boolean,
+      default: false
+    },
+    deletedAt: Date,
+    deletedBy: String,
+    createdBy: String,
+    updatedBy: String,
+  }, {
+    timestamps: true, //updatedAt and createdAt
+  }
+)
+
+const AccountAdmin = mongoose.model("AccountAdmin", schema, "account-admin");
+
+export default AccountAdmin;
