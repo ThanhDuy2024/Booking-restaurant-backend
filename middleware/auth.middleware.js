a5b459cbf4f2bf632cddfef5806cebdd07afe224
Duy Lâm <thanhduyuth@gmail.com>
2025-05-19 10:31:05 +0700

    Phân quyền chức năng cơ bản trang admin

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,38 @@
+import AccountAdmin from "../models/accountAdmin.model.js";
+import jwt from "jsonwebtoken";
+
+export const authentication = async (req, res, next) => {
+  if (req.cookies.authToken) {
+    const token = req.cookies.authToken
+    const decoded = jwt.verify(token, process.env.PRIVATE_KEY);
+
+    if (!decoded.email) {
+      res.clearCookie("authToken");
+      res.status(401).json({
+        message: "Bạn không có quyền truy cập vào chức năng này"
+      })
+      return;
+    }
+
+    const accountAdmin = await AccountAdmin.findOne({
+      email: decoded.email,
+      deleted: false,
+      status: "active"
+    })
+
+    if (!accountAdmin) {
+      res.clearCookie("authToken");
+      res.status(401).json({
+        message: "Bạn không có quyền truy cập vào chức năng này"
+      })
+      return;
+    }
+    req.accountAdmin = accountAdmin.role;
+  } else {
+    res.status(401).json({
+      message: "Bạn không có quyền truy cập vào chức năng này"
+    })
+    return;
+  }
+  next();
+}
--- router/admin/index.route.js
+++ router/admin/index.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import authRouter from "./auth.route.js";
 import categoryRouter from "./category.route.js";
-
+import { authentication } from "../../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.use("/auth", authRouter);
 
-router.use('/category', categoryRouter);
+router.use('/category',authentication, categoryRouter);
 
 export default router;
