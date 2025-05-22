8effa86b48ea44e088d430998ebd8c75dc52f2de
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-05-22 10:31:41 +0700

    Api chỉnh sửa chi nhánh

--- controller/admin/branch.controller.js
+++ controller/admin/branch.controller.js
@@ -30,4 +30,45 @@ export const branchCreateController = async (req, res) => {
   }
 }
 
+export const branchEditController = async (req, res) => {
+  if(req.accountAdmin.role != "admin") {
+    res.status(401).json({
+      message: "Bạn không có quyền truy cập vào trức năng này"
+    })
+    return;
+  }
+  try {
+    const id = req.params.id;
+    const branch = await Branch.findOne({
+      _id: id,
+      status: "active",
+      deleted: false
+    });
 
+    if(!branch) {
+      res.status(404).json({
+        message: "Không tìm thấy thông tin chi nhánh",
+      })
+      return;
+    }
+
+    if(req.file) {
+      req.body.avatar = req.file.path;
+    } else {
+      delete req.body.avatar;
+    }
+
+    await Branch.updateOne({
+      _id: branch.id
+    }, req.body);
+    
+    res.status(200).json({
+      message: "Cập nhật thông tin chi nhánh thành công",
+    })
+    
+  } catch (error) {
+    res.status(400).json({
+      message: "Cập nhật thông tin chi nhánh thất bại",
+    })
+  }
+}
--- router/admin/branch.route.js
+++ router/admin/branch.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { branchCreateController } from "../../controller/admin/branch.controller.js";
+import { branchCreateController, branchEditController } from "../../controller/admin/branch.controller.js";
 const router = express.Router();
 import storage from "../../helpers/cloudinaryHelper.js";
 
@@ -8,6 +8,8 @@ const upload = multer({
   storage: storage
 })
 
-router.post('/create',upload.single("avatar"), branchCreateController);
+router.post('/create', upload.single("avatar"), branchCreateController);
+
+router.post('/edit/:id', upload.single("avatar"), branchEditController);
 
 export default router;
