3c88c10837d99e9ce5e74f71f94481be1919eb17
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-06-14 16:44:44 +0700

    Api lấy ra số lượng trạng thái đơn hàng theo chi nhánh và tổng số tiền tất cả chi nhánh và từng chi nhánh

--- controller/admin/dashboard.controller.js
+++ controller/admin/dashboard.controller.js
@@ -16,10 +16,74 @@ export const orderStatusAllBranch = async (req, res) => {
     status: "cancel",
     deleted: false
   })
-  
+
   res.status(200).json({
     inital: recordInital,
     completed: recordCompleted,
     cancel: recordCancel
+  })
+}
+
+export const orderStatusBranch = async (req, res) => {
+
+  const recordInital = await Order.countDocuments({
+    status: "inital",
+    deleted: false,
+    branchId: req.accountAdmin.branch
+  })
+
+  const recordCompleted = await Order.countDocuments({
+    status: "completed",
+    deleted: false,
+    branchId: req.accountAdmin.branch
+  })
+
+  const recordCancel = await Order.countDocuments({
+    status: "cancel",
+    deleted: false,
+    branchId: req.accountAdmin.branch
+  })
+
+  res.status(200).json({
+    inital: recordInital,
+    completed: recordCompleted,
+    cancel: recordCancel
+  })
+}
+
+export const orderPriceAllBranch = async (req, res) => {
+  let price = 0;
+
+  const record = await Order.find({
+    status: "completed",
+    deleted: false
+  });
+
+  for (const item of record) {
+    price += item.totalPrice;
+  }
+
+  const priceFormat = price.toLocaleString("vi-VN");
+  res.status(200).json({
+    totalPriceAllBranch: priceFormat,
+  })
+}
+
+export const orderPriceBranch = async (req, res) => {
+  let price = 0;
+
+  const record = await Order.find({
+    status: "completed",
+    deleted: false,
+    branchId: req.accountAdmin.branch
+  });
+
+  for (const item of record) {
+    price += item.totalPrice;
+  }
+
+  const priceFormat = price.toLocaleString("vi-VN");
+  res.status(200).json({
+    totalPriceBranch: priceFormat,
   })
 }
--- router/admin/dashboarch.route.js
+++ router/admin/dashboarch.route.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
-import { orderStatusAllBranch } from "../../controller/admin/dashboard.controller.js";
+import { orderStatusAllBranch, orderStatusBranch, orderPriceAllBranch, orderPriceBranch} from "../../controller/admin/dashboard.controller.js";
 
 const router = Router();
 
-router.get('/orderAllBranch', orderStatusAllBranch)
+router.get('/order/status/all/branch', orderStatusAllBranch);
+
+router.get('/order/status/branch', orderStatusBranch);
+
+router.get('/total/price/all/branch', orderPriceAllBranch);
+
+router.get('/total/price/branch', orderPriceBranch);
 export default router;
