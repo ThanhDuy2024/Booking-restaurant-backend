f955354c64d2d5779445a89d6d9bca902446c162
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-06-28 20:22:09 +0700

    Sửa cookie

--- controller/admin/auth.controller.js
+++ controller/admin/auth.controller.js
@@ -38,7 +38,7 @@ export const loginController = async (req, res) => {
   res.cookie("authToken", authToken, {
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    secure: process.env.ENVIRONMENT_SECURE == "production" ? true : false, //false: http, true: https
+    secure: true,
     sameSite: "none" //cho phep gui cookie giua cac domain
   });
   
